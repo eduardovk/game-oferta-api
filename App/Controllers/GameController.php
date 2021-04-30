c3224568588488aac97fbe587e7712fb23b477aa
eduardovk <eduardo_vk@hotmail.com>
2021-04-30 18:27:03 -0300

    Adicionada rota de game_deals e refeito get de jogo

--- App/Controllers/GameController.php
+++ App/Controllers/GameController.php
@@ -15,18 +15,60 @@ final class GameController
     {
         $gameDAO = new GameDAO();
         $games = $gameDAO->getAllGames();
+        $res = $res->withJson($games);
+        return $res;
+    }
 
+    //retorna informacoes sobre o jogo atraves da plain informada
+    public function getGameByPlain(Request $req, Response $res, array $args): Response
+    {
+        $gameDAO = new GameDAO();
+        $plain = $args['plain']; //recebe plain como parametro get da url
+        $games = $gameDAO->getGameByPlain($plain);
         $res = $res->withJson($games);
         return $res;
     }
 
     //retorna o jogo informado pela plain e suas ofertas ativas
-    public function getGameByPlain(Request $req, Response $res, array $args): Response
+    public function getGameDealsByPlain(Request $req, Response $res, array $args): Response
     {
+        $plain = $args['plain']; //recebe plain como parametro get da url
+        //array com informacoes do jogo
+        $gameDeals =  array(
+            'plain' => $plain,
+            'name' => '',
+            'id' => '',
+            'cover' => '',
+            'deals' => array() //array de ofertas do jogo
+        );
         $gameDAO = new GameDAO();
-        $game = $gameDAO->getGameByPlain($args['plain']);
-
-        $res = $res->withJson($game);
+        //caso haja parametro all_deals e seja true, retorna todas as deals
+        $params = $req->getQueryParams();
+        $allDeals = false;
+        if (isset($params['all_deals']) && $params['all_deals'] == 'true') $allDeals = true;
+        $gameInfo = $gameDAO->getGameByPlain($plain); //pega informacoes do jogo no bd
+        if ($gameInfo) { //se encontrar o jogo no bd
+            //preenche array com informacoes do jogo
+            $gameDeals['name'] = $gameInfo['name'];
+            $gameDeals['id'] = $gameInfo['id'];
+            $gameDeals['cover'] = 'https:' . $gameInfo['igdb_cover'];
+            $deals = $gameDAO->getGameDealsByPlain($plain, $allDeals); //busca as deals do jogo
+            foreach ($deals as $deal) { //para cada deal encontrada
+                //seleciona informacoes relevantes da deal e insere no array
+                $dealInfo = array(
+                    'id_deal' => $deal['id'],
+                    'id_store' => $deal['id_store'],
+                    'store_name' => $deal['store_name'],
+                    'store_plain' => $deal['id_itad'],
+                    'price_old' => $deal['price_old'],
+                    'price_new' => $deal['price_new'],
+                    'price_cut' => $deal['price_cut']
+                );
+                //insere array da deal no array principal
+                array_push($gameDeals['deals'], $dealInfo);
+            }
+        }
+        $res = $res->withJson($gameDeals);
         return $res;
     }
 }
--- App/DAO/GameDAO.php
+++ App/DAO/GameDAO.php
@@ -15,24 +15,37 @@ class GameDAO extends Connection
         $games = $this->pdo
             ->query('SELECT * FROM games WHERE active = 1 ORDER BY id DESC')
             ->fetchAll(\PDO::FETCH_ASSOC);
-
         return $games;
     }
 
-    //retorna o jogo informado pela plain e suas ofertas ativas
+    //retorna todos os campos de um jogo a partir de sua plain
     public function getGameByPlain($plain)
     {
-        //verifica se ha  variavel de ambiente para filtrar as lojas
+        //seleciona jogo cuja plain nao seja duplicada ou nao conferida
+        $query = $this->pdo->prepare('SELECT * FROM games WHERE active = 1 AND '
+            . '(duplicate_plain IS NULL OR duplicate_checked IS NOT NULL) AND plain  = :plain');
+        $query->bindValue(':plain', $plain);
+        $run = $query->execute();
+        $game = $query->fetchAll(\PDO::FETCH_ASSOC);
+        if ($game) return $game[0];
+        return null;
+    }
+
+    //retorna o jogo informado pela plain e suas deals
+    //caso $allDeals seja true, retorna todas deals
+    //caso seja false, retorna apenas as deals ativas
+    public function getGameDealsByPlain($plain, $allDeals)
+    {
+        //verifica se ha variavel de ambiente para filtrar as lojas
         $where = "";
         if (getenv('FILTER_STORES') && getenv('FILTER_STORES') != "") {
             $where = "AND id_store IN (" . getenv('FILTER_STORES') . ")";
         }
-
-        $query = $this->pdo->prepare('SELECT * FROM game_deals WHERE game_plain = :plain ' . $where);
+        $table = $allDeals ? 'game_all_deals' : 'game_deals';  //seleciona entre uma das views
+        $query = $this->pdo->prepare('SELECT * FROM ' . $table . ' WHERE game_plain = :plain ' . $where);
         $query->bindValue(':plain', $plain);
         $run = $query->execute();
         $game = $query->fetchAll(\PDO::FETCH_ASSOC);
-
         return $game;
     }
 }
